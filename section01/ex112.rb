9103562199368e1faf10771042aaaece036de728
Ebikatsu <tohenchokuju@outlook.jp>
2017-08-07 00:51:27 +0900

    いろいろなif文

--- section01/ex112.rb
+++ section01/ex112.rb
@@ -1,14 +1,16 @@
 a = 20
+print "#{a}は "
 if a>=10 then
-    puts "greater"
+    puts "10以上"
 end
 if a<=9 then
-    puts "smaller"
+    puts "9以下"
 end
 
+print "#{a}は "
 if a>=10 then
-    puts "greater"
+    puts "10以上"
 else
-    puts "smaller"
+    puts "10未満"
 end
 
