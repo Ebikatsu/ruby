2190e1aef5421452c3c5c231869d28073297fdee
Ebikatsu <tohenchokuju@outlook.jp>
2017-08-07 00:44:41 +0900

    関数

--- section01/ex083.rb
+++ section01/ex083.rb
@@ -1,3 +1,3 @@
-print "Math.sin(3.14)\n","=",Math.sin(3.14), "\n"
-print "Math.cos(3.14)\n","=",Math.cos(3.14), "\n"
-print "Math.sqrt(121)\n","=",Math.sqrt(121), "\n"
+print "Math.sin(3.14)\n","= ",Math.sin(3.14), "\n"
+print "Math.cos(3.14)\n","= ",Math.cos(3.14), "\n"
+print "Math.sqrt(121)\n","= ",Math.sqrt(121), "\n"
