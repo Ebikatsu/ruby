86699dd12e304306b26bfc4d92d64390483cc863
Ebikatsu <tohenchokuju@outlook.jp>
2017-08-09 22:34:28 +0900

    caseの使い方

---
+++ section05/ex04.rb
@@ -0,0 +1,13 @@
+color = ARGV[0]
+
+case color
+when "red"
+    puts "Don't across!"
+when "yello"
+    puts "stop!"
+when "green"
+    puts "Go!"
+else
+    puts "It is not color for traffic signal."
+end
+
