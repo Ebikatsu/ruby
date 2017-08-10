be977a858f630613c8656d5258b1f48d6084587b
Ebikatsu <s153109@matsu.shimane-u.ac.jp>
2017-08-10 23:04:11 +0900

    第8章

---
+++ section08/ex0101.rb
@@ -0,0 +1,17 @@
+ary = Array.new
+p ary
+puts
+
+str = "Hello World!"
+p ary.class
+p str.class
+puts
+
+p ary.instance_of?(Array)
+p str.instance_of?(Array)
+p ary.instance_of?(String)
+p str.instance_of?(String)
+p ary.instance_of?(Integer)
+p str.instance_of?(Integer)
+puts
+
---
+++ section08/ex0102.rb
@@ -0,0 +1,5 @@
+str2 = "This is a String."
+p str.is_a?(String)
+p str.is_a?(Integer)
+p str.is_a?(Object)
+
