2bdedd653ec91a86e728eb12a5b22a60f4d99491
Ebikatsu <s153109@matsu.shimane-u.ac.jp>
2017-08-10 23:12:55 +0900

    クラスとインスタンス

--- section08/ex0101.rb
+++ section08/ex0101.rb
@@ -1,17 +1,16 @@
 ary = Array.new
-p ary
+puts ary
 puts
 
 str = "Hello World!"
-p ary.class
-p str.class
+puts "ary.class:#{ary.class}"
+puts "str.class:#{str.class}"
 puts
 
-p ary.instance_of?(Array)
-p str.instance_of?(Array)
-p ary.instance_of?(String)
-p str.instance_of?(String)
-p ary.instance_of?(Integer)
-p str.instance_of?(Integer)
-puts
+puts "ary.instance_of?(Array):#{ary.instance_of?(Array)}"
+puts "str.instance_of?(Array):#{str.instance_of?(Array)}"
+puts "ary.instance_of?(String):#{ary.instance_of?(String)}"
+puts "str.instance_of?(String):#{str.instance_of?(String)}"
+puts "ary.instance_of?(Integer):#{ary.instance_of?(Integer)}"
+puts "str.instance_of?(Integer):#{str.instance_of?(Integer)}"
 
